485449a839036d6d4c0ef4bceb0a9972f853b0e9
Micheal Singleton <micwsing@gmail.com>
2017-09-09 14:54:00 -0400

    Added data functions in blank_page index.js. Exported data members from components/config files

--- js/components/blankPage/index.js
+++ js/components/blankPage/index.js
@@ -4,6 +4,10 @@ import { Image, View } from 'react-native';
 import { connect } from 'react-redux';
 import { Actions } from 'react-native-router-flux';
 import { Container, Header, Content, Button, Icon, Left, Body, Right } from 'native-base';
+var accessToken = require('../config/strings');
+var rider1 = require('../config/rider1');
+var rider2 = require('../config/rider2');
+var rider3 = require('../config/rider3');
 
 import { openDrawer } from '../../actions/drawer';
 // import HeaderContent from './../headerContent/';
@@ -49,6 +53,83 @@ class BlankPage extends Component { // eslint-disable-line
         </Image>
       </Container>
     );
+  }
+}
+
+componentDidMount() {
+    fetch('http://developer.itsmarta.com/RealtimeTrain/RestServiceNextTrain/GetRealtimeArrivals?apikey=eda9f740-8b7e-4071-a680-821a1c3396fe', {
+        method: 'GET'
+    })
+        .then((response) => {
+            console.log(response._bodyInit);
+        })
+        .catch((error) => {
+            console.error(error);
+        });
+
+    //Call for rider1
+    fetch('https://sandbox-api.uber.com/v1.2/requests/estimate', {
+        method: 'POST',
+        headers: {
+            'Authorization': accessToken,
+            'content-type': 'application/json'
+        },
+        body: JSON.stringify({
+            "start_latitude": rider1.start_latitude,
+            "start_longitude": rider1.start_longitude,
+            "end_latitude": rider1.end_latitude,
+            "end_longitude": rider1.end_longitude
+        })
+    })
+    .then((response) => {
+        console.log(response._bodyInit);
+    })
+    .catch((error) => {
+        console.error(error);
+    });
+
+    //Call for rider2
+    fetch('https://sandbox-api.uber.com/v1.2/requests/estimate', {
+        method: 'POST',
+        headers: {
+            'Authorization': accessToken,
+            'content-type': 'application/json'
+        },
+        body: JSON.stringify({
+            "start_latitude": rider2.start_latitude,
+            "start_longitude": rider2.start_longitude,
+            "end_latitude": rider2.end_latitude,
+            "end_longitude": rider2.end_longitude
+        })
+    })
+    .then((response) => {
+        console.log(response._bodyInit);
+    })
+    .catch((error) => {
+        console.error(error);
+    });
+
+    //Call for rider3
+    fetch('https://sandbox-api.uber.com/v1.2/requests/estimate', {
+        method: 'POST',
+        headers: {
+            'Authorization': accessToken,
+            'content-type': 'application/json'
+        },
+        body: JSON.stringify({
+            "start_latitude": rider3.start_latitude,
+            "start_longitude": rider3.start_longitude,
+            "end_latitude": rider3.end_latitude,
+            "end_longitude": rider3.end_longitude
+        })
+    })
+    .then((response) => {
+        console.log(response._bodyInit);
+    })
+    .catch((error) => {
+        console.error(error);
+    });
+    }
   }
 }
 
---
+++ js/components/config/rider3.js
@@ -0,0 +1,16 @@
+var rider3 = {
+    "firstname": "Lady",
+    "lastname": "Mormon",
+    "homeAdress": "265 Park Ave W NW, Atlanta, GA 30313",
+    "workAdress": "441 16th St NW, Atlanta, GA 30363",
+    "workload": "20",
+    "workstart": "7:00AM",
+    "workend": "6:00PM",
+    "priceceiling": "7.00",
+    "start_latitude": "33.761837",
+    "start_longitude": "-84.394510",
+    "end_latitude": "33.788796",
+    "end_longitude": "-84.405279"
+}
+
+module.exports = rider3; 
